f617d0aed682535d8a1f302254cfde6380c16306
PaddingE <131670101+PaddingE@users.noreply.github.com>
2023-04-28 17:52:38 +0900

    Add files via upload

---
+++ samplecode_0428/day0428_03_stack02.py
@@ -0,0 +1,72 @@
+import webbrowser
+import time
+
+# test
+def is_stack_full():
+    global SIZE, stack, top
+    if(top >= SIZE - 1):
+        return True
+    else:
+        return False
+   
+def is_stack_empty():
+    global SIZE, stack, top
+    if (top == -1):
+        return True
+    else:
+        return False
+
+def push(data):
+    global SIZE, stack, top
+    if (is_stack_full()):
+        print("스택이 꽉 찼습니다.")
+        return
+    top += 1
+    stack[top] = data
+ 
+def pop():
+    global SIZE, stack, top
+    
+    if(is_stack_empty()):
+        print("스택이 비어있습니다.")
+        return None
+    data = stack[top]
+    stack[top] = None
+    top -= 1
+    return data
+
+def peek():
+    global SIZE, stack, top
+    
+    if (is_stack_empty()):
+        print("스택이 비었습니다.")
+        return None
+    
+    return stack[top]
+
+SIZE = 100
+stack = [None for _ in range(SIZE)]
+top = -1
+
+if __name__ == "__main__":
+    urls = ['naver.com', 'daum.net', 'nate.com']
+    
+    for url in urls:
+        push(url)
+        webbrowser.open('http://'+url)
+        print(url, end= '-->')
+        time.sleep(1)
+        
+    print("방문 종료")
+    time.sleep(5)
+    
+    while True:
+        url = pop()
+        if url == None:
+            break
+        
+        webbrowser.open('http://'+url)
+        
+        print(url, end='-->')
+        time.sleep(1)
+    print("방문 종료")
